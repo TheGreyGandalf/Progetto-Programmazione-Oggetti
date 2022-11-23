0c84c0bbbc6193368dffbe7c7b3051f60434adff
Gandalf <118108075+TheGreyGandalf@users.noreply.github.com>
2022-11-23 21:41:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame f = new JFrame("Programma Esame Oggetti");
+        Conto [] listaConto = new Conto [3];
+
+        listaConto[0] = new Conto("Giacomo", "Cabri", 12345);
+        listaConto[1] = new Conto("Mario", "Rossi", 543345);
+        listaConto[2] = new Conto("Carlo", "Bianchi", 25844);
+        MyPanel panel = new MyPanel(listaConto);
+        f.add(panel);
+        f.pack();
+        //f.setBounds(300,300, 300,300);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+
+        f.setVisible(true);
     }
 }
