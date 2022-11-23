8eeb8d3f5e5b99cfbc43f1d941062cf291c4cec7
Gandalf <118108075+TheGreyGandalf@users.noreply.github.com>
2022-11-23 17:10:03 +0100

    File iniziali

---
+++ Conto.java
@@ -0,0 +1,11 @@
+public class Conto {
+    public String Data;
+    public String Descrizione;
+    public int Ammontare;
+
+    Conto(String Da, String De, int Am){
+        this.Data=Da;
+        this.Descrizione=De;
+        this.Ammontare=Am;
+    }
+}
