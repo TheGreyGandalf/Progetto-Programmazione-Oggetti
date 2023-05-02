0fe196e2353613e15de2d32b2945607e42c861c2
Gandalf <118108075+TheGreyGandalf@users.noreply.github.com>
2023-05-02 20:32:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Progetto creato in Java per il corso di Programmazione ad Oggetti
+Il progetto deve poter gestire un bilancio aziendale e poter effettuare modifiche ad esso
+L'Esportazione in Open document dipende dalla libreria JOpenDocument
---
+++ compilazione.sh
@@ -0,0 +1,3 @@
+javac -cp "./jOpenDocument.jar" Main/Main.java Scrittura_File/ScritturaFile.java Scrittura_File/ScriviCsv.java Struttura/tab.java Struttura/MyPanel.java Classe_Conto/Conto.java Struttura/CalcolaEntrate.java
+java Main/Main
+rm Classe_Conto/*.class Main/*.class Scrittura_File/*.class Struttura/*.class
