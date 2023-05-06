88c229fcb6d5b3bec3eac771e6f8bb7cb9af7ec1
Gandalf <118108075+TheGreyGandalf@users.noreply.github.com>
2023-05-06 19:01:25 +0200

    Add files via upload

--- compilazione.sh
+++ compilazione.sh
@@ -1,5 +1,5 @@
 declare i=${1:-}
-javac -cp "./jOpenDocument.jar" Main/Main.java Scrittura_File/ScritturaFile.java Scrittura_File/ScriviCsv.java Struttura/tab.java Struttura/MyPanel.java Classe_Conto/Conto.java Struttura/CalcolaEntrate.java
-java Main/Main $i
+javac -cp ":./jOpenDocument.jar:org.jopendocument.dom.spreadsheet.jar:org.jopendocument.dom.OOUtils.jar" Main/Main.java Scrittura_File/ScritturaFile.java Scrittura_File/ScriviCsv.java Struttura/tab.java Struttura/MyPanel.java Classe_Conto/Conto.java Struttura/CalcolaEntrate.java
+java -cp ":./jOpenDocument.jar:org.jopendocument.dom.spreadsheet.jar:org.jopendocument.dom.OOUtils.jar" Main/Main $i
 rm Classe_Conto/*.class Main/*.class Scrittura_File/*.class Struttura/*.class
 unset i
